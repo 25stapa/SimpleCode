d885c32f6b7764a7a8d81f560e1c7e0c88366f16
stapa <stapa@me.com>
2023-01-29 00:37:45 +0300

    add Lesson 01; 04; 05; 06

---
+++ Lesson05/Program.cs
@@ -0,0 +1,9 @@
+﻿/*
+*   Ввод данных в консоль
+ */
+string data;
+
+data = Console.ReadLine();
+
+Console.WriteLine("Hi, " + data + "!!!");
+
---
+++ Lesson06/Program.cs
@@ -0,0 +1,54 @@
+﻿/*
+ *  Конвертация строки
+ *  класс Convert
+ */
+
+ 
+
+// string str;
+// int  a, b;
+
+// Console.WriteLine("Input numbers 1 ");
+// str = Console.ReadLine();
+// a = Convert.ToInt32(str);
+
+// Console.WriteLine("Input numbers 2 ");
+// str = Console.ReadLine();
+// b = Convert.ToInt32(str);
+
+// int result = a + b;
+
+// Console.WriteLine("Сумма чисел = " + result);
+
+
+Console.WriteLine("Input first numbers ");
+string a = Console.ReadLine();
+
+int b = Convert.ToInt32(a);
+Console.WriteLine("Input second numbers ");
+string c = Console.ReadLine();
+int d = Convert.ToInt32(c);
+Console.WriteLine("result = " + (b+d));
+
+
+
+
+// string str = "1,9";
+// double a = Convert.ToDouble(str);
+
+// Console.WriteLine(a);
+
+
+//  Если вместо запятой будет точка, тогда на выводе в консоль будет ошибка. Тогда ...
+
+
+// string str = "1.9";
+
+// System.Globalization.NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+// {
+//     NumberDecimalSeparator = ".",
+// };
+
+// double a = Convert.ToDouble(str, numberFormatInfo);
+
+// Console.WriteLine(a);
